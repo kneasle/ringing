995b8bbbee065dd57f158da939897f6d37a0decf
Kneasle <kneasle@gmail.com>
2021-06-28 17:33:42 +0100

    Parse CLI args

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,27 @@
+use std::path::PathBuf;
+
 use monument_cli::spec::Spec;
+use structopt::StructOpt;
+
+/// A struct storing the CLI args taken by Monument.  `StructOpt` will generate the argument
+/// parsing/help code for us.
+#[derive(Debug, Clone, StructOpt)]
+#[structopt(name = "Monument", about = "A music-oriented composing engine")]
+struct CliArgs {
+    /// The name of the `.toml` file for Monument to generate
+    #[structopt(parse(from_os_str))]
+    input_file: PathBuf,
+
+    /// The maximum number of threads that Monument will use
+    #[structopt(short = "T", long)]
+    num_threads: Option<usize>,
+}
 
 fn main() {
-    // let file_name = "test-cases/bristol-s10-lb4s.toml";
-    // let file_name = "test-cases/bristol-s12-qps.toml";
-    let file_name = "test-cases/yorkshire-s10-qps.toml";
-    // let file_name = "test-cases/superlative-s8-qps.toml";
+    // Parse the CLI args
+    let args = CliArgs::from_args();
 
-    let spec_toml = std::fs::read_to_string(file_name).unwrap();
+    let spec_toml = std::fs::read_to_string(args.input_file).unwrap();
     let spec: Spec = toml::from_str(&spec_toml).unwrap();
     println!("{:#?}", spec);
 
