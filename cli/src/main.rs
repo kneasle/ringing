c56df1980d88d9765ad406e9444d98f563fcd488
Kneasle <kneasle@gmail.com>
2021-07-11 16:00:31 +0100

    Print nodes/s and comps/s

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use log::LevelFilter;
 use monument::Config;
 use monument_cli::{args::CliArgs, spec::AbstractSpec};
+use number_prefix::NumberPrefix;
 use structopt::StructOpt;
 
 /// Entry point for the CLI command `monument`
@@ -27,9 +28,24 @@ fn main() {
     let results = monument::compose(&comp_spec, &config);
 
     // Print the results
-    println!("Completed in {:.2?}", results.time_taken);
+    let nodes_per_sec = results.stats.nodes_considered as f64 / results.time_taken.as_secs_f64();
+    let comps_per_sec = results.stats.comps_found as f64 / results.time_taken.as_secs_f64();
+    println!(
+        "Completed in {:.2?} ({} nodes/s, {} comps/s)",
+        results.time_taken,
+        fmt_with_prefix(nodes_per_sec),
+        fmt_with_prefix(comps_per_sec)
+    );
     dbg!(&results.stats);
     for c in results.comps {
         println!("{}", c.to_string(comp_spec.layout()));
+    }
+}
+
+/// Format a big number with a suffix
+fn fmt_with_prefix(num: f64) -> String {
+    match NumberPrefix::decimal(num) {
+        NumberPrefix::Standalone(n) => format!("{:.3}", n),
+        NumberPrefix::Prefixed(prefix, n) => format!("{:.1}{}", n, prefix),
     }
 }
